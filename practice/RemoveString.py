9772c9a8756eec2a58edb480edb1ed51a2957637
SarthakMishra <74105574+SarthakMishra0307@users.noreply.github.com>
2023-04-22 10:17:48 +0530

    Update RemoveString.py

--- practice/RemoveString.py
+++ practice/RemoveString.py
@@ -1,2 +1,13 @@
-s = "leet**cod*e"
-for i in range(len())
+# s = "left**cod*e"
+# s = "erase*****"
+
+new_s = ""
+arr= []
+for i in range(len(s)):
+    if s[i] == "*":
+        arr.pop()
+    else:
+        arr.append(s[i])
+
+for i in range(len(arr)):
+    new_s += arr[i]
